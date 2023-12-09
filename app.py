e0d750da74c45dc44ee9102e18777271dca0801c
daffa <draihandika@gmail.com>
2023-12-10 02:32:24 +0700

    predict video yolov8

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import numpy as np
 from PIL import Image
 import preditct_model
 import os
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, send_file
 from datetime import datetime
 from functools import wraps, update_wrapper
 from shutil import copyfile
 import random
-
+from werkzeug.utils import secure_filename
+import time
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -52,25 +54,67 @@ def add_header(r):
     return r
 
 
-@app.route("/upload", methods=["POST"])
-@nocache
-def upload():
-    target = os.path.join(APP_ROOT, "static/img")
-    if not os.path.isdir(target):
-        if os.name == 'nt':
-            os.makedirs(target)
-        else:
-            os.mkdir(target)
-    for file in request.files.getlist("file"):
-        file.save("static/img/img_now.jpg")
-    copyfile("static/img/img_now.jpg", "static/img/img_normal.jpg")
-    return render_template("uploaded.html", file_path="img/img_now.jpg")
-
 @app.route("/detection-yolo", methods=["POST"])
 @nocache
 def detection_yolo():
     preditct_model.detection_yolo()
     return render_template("uploaded.html", file_path="img/img_now.jpg")
+
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_VIDEO_EXTENSIONS = {'mp4', 'avi', 'mkv', 'mov'}
+
+def allowed_file(filename, allowed_extensions):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def run_detection():
+    try:
+        with app.app_context():
+            preditct_model.detection_yolo_video()
+    except Exception as e:
+        print(f"Error in run_detection: {e}")
+    
+@app.route("/upload", methods=["POST"])
+@nocache
+def upload():
+    target = os.path.join(APP_ROOT, "static")
+
+    if not os.path.exists(os.path.join(target, "img")):
+        os.makedirs(os.path.join(target, "img"))
+
+    if not os.path.exists(os.path.join(target, "videos")):
+        os.makedirs(os.path.join(target, "videos"))
+
+    for file in request.files.getlist("file"):
+        if file:
+            filename = secure_filename(file.filename)
+            if allowed_file(filename, ALLOWED_VIDEO_EXTENSIONS):
+                file_path = os.path.join(target, "videos", "video_now.mp4")
+                file.save(file_path)
+
+                run_detection()
+                
+                time.sleep(2)
+
+                return render_template("uploaded_video.html", file_path=file_path)
+            elif allowed_file(filename, ALLOWED_IMAGE_EXTENSIONS):
+                file_path = os.path.join(target, "img", "img_now.jpg")
+                file.save(file_path)
+
+                # Copy image for rendering in uploaded.html
+                copyfile(file_path, os.path.join(target, "img", "img_normal.jpg"))
+                return render_template("uploaded.html", file_path=file_path)
+
+    return "Invalid file format. Allowed formats: mp4, png, jpg, jpeg, gif"
+
+@app.route('/static/videos/video_result.mp4')
+def get_video_result():
+    video_result_path = os.path.join(APP_ROOT, 'static', 'videos', 'video_result.mp4')
+    response = send_file(video_result_path, mimetype='video/mp4')
+    response.headers.add('Accept-Ranges', 'bytes')
+    response.headers.add('Content-Length', os.path.getsize(video_result_path))
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
--- preditct_model.py
+++ preditct_model.py
@@ -1,4 +1,6 @@
 from ultralyticsplus import YOLO, render_result
+import cv2
+import numpy as np
 # load model
 model = YOLO("best.pt")
 
@@ -10,6 +12,8 @@ model.overrides['max_det'] = 1000  # maximum number of detections per image
 
 # inference
 image_path = "static/img/img_normal.jpg"
+input_video_path = "static/videos/video_now.mp4"
+output_video_path = "static/videos/video_result.mp4"
 
 def detection_yolo():
     results = model.predict(image_path)
@@ -17,3 +21,36 @@ def detection_yolo():
     print(results[0].boxes)
     render = render_result(model=model, image=image_path, result=results[0])
     render.save("static/img/img_now.jpg")
+
+def detection_yolo_video():
+    cap = cv2.VideoCapture(input_video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert the frame to a NumPy array
+        frame_np = np.array(frame)
+
+        # Make a copy of the frame to avoid modifying the original
+        frame_copy = frame_np.copy()
+
+        results = model.predict(frame_copy)
+        render = render_result(model=model, image=frame_copy, result=results[0])
+        
+        # Convert render back to BGR format before writing
+        render_bgr = cv2.cvtColor(np.array(render), cv2.COLOR_RGB2BGR)
+        out.write(render_bgr)
+
+    cap.release()
+    out.release()
+
+
+
